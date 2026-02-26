838aabf7d087dbed78cc68d271fa0dd9dc662b2e
siddiqua712-gif <nsiddiqua712@gmail.com>
2026-02-27 03:20:47 +0530

    fixed

---
+++ models/user.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()  # Use the same db instance in your main app if you already have one
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
